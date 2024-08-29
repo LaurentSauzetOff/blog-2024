fe6de52b5140c9b1d4732d7fc75f657c895970f8
SAUZET Laurent <laurent.sauzetpro@gmail.com>
2024-08-29 15:23:26 +0200

    Footer component

--- src/app/layout.js
+++ src/app/layout.js
@@ -2,6 +2,7 @@ import './globals.css';
 import { Inter, Manrope } from 'next/font/google';
 import { cx } from "../utils/index";
 import Header from "@/src/components/Header";
+import Footer from './../components/Footer/Footer';
 
 const inter = Inter({ subsets: ['latin'], display: "swap", variable: "--font-im" })
 const manrope = Manrope({ subsets: ['latin'], display: "swap", variable: "--font-mr"  })
@@ -16,7 +17,7 @@ export default function RootLayout({ children }) {
     <html lang="en">
       <body 
       className={cx(inter.variable, manrope.variable, "font-mr bg-light dark:bg-dark")}
-      ><Header />{children}</body>
+      ><Header />{children}<Footer /></body>
     </html>
   )
 }
